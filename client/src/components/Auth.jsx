1614c9df8bef34e7c7554e77e14e9df41fd79470
Abdusamadbek Axmadjonov <itstianmi@gmail.com>
2022-12-11 19:26:34 +0500

    predeploy

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -29,7 +29,7 @@ function Auth() {
 
         const { username, password, phoneNumber, avatarURL } = form
 
-        const URL = 'http://localhost:5000/auth/'
+        const URL = 'https://chat-stream.vercel.app/auth/'
 
         const { data: {token, userId, hashedPassword, fullName} } = await axios.post(`${URL}/${isSignUp ? 'signup' : 'login'}`, {
             fullName : form.fullName, username, password, phoneNumber, avatarURL
