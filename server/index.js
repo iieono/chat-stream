ee260cc0747d95ecc094ce0a5ed76e1d77ceef4c
Abdusamadbek Axmadjonov <itstianmi@gmail.com>
2022-12-11 19:35:13 +0500

    predeploy

--- server/index.js
+++ server/index.js
@@ -18,6 +18,13 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded())
 
+app.use((req, res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+    next(); 
+})
+
 app.get('/', (req, res)=>{
     res.send('Server Online')
 })
